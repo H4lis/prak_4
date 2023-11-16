5d8136eeb82c59d7842b7beb45c492bdf5db7670
H4lis <nurhalis092@gmail.com>
2023-11-16 16:01:00 +0800

    first commit

---
+++ app/src/main/java/com/dicoding/myrecyclerview/DetailScreen.kt
@@ -0,0 +1,13 @@
+package com.dicoding.myrecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_screen)
+
+    }
+}
